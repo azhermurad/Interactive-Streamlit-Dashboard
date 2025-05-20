485d9c02bd6c30fb1830aefd73524e58f68b29cf
MahakKhalid747 <mahakkhalid747@gmail.com>
2025-05-20 19:10:11 +0500

    added page_five to app.py

--- app.py
+++ app.py
@@ -12,7 +12,8 @@ pages = [
         st.Page("pages/page_one.py", title="Home",icon=":material/home:"),
         st.Page("pages/page_two.py", title="Page_2",icon=":material/responsive_layout:"),
         st.Page("pages/page_three.py", title="Page_3",icon=":material/monitoring:"),
-        st.Page("pages/page_four.py", title="Page_4",icon=":material/map:")]
+        st.Page("pages/page_four.py", title="Page_4",icon=":material/map:"),
+        st.Page("pages/page_five.py", title="Page_5",icon=":material/map:")]
 
 
 pg = st.navigation(pages)
