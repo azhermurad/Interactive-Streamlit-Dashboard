9165e56b14c98c08c9d151ea9c5774fac412fc8c
azher ali <azhermurad@gmail.com>
2025-05-21 13:43:46 +0500

    page icons add to slidebar

--- app.py
+++ app.py
@@ -9,12 +9,28 @@ st.set_page_config(page_title="Streamlit Dashboard (EDA) Project", layout="wide"
 
 
 pages = [
-        st.Page("pages/page_one.py", title="Home",icon=":material/home:"),
-        st.Page("pages/page_two.py", title="Correlation between Features",icon=":material/responsive_layout:"),
-        st.Page("pages/page_three.py", title="Analysis based on Bill Length & Bill Depth",icon=":material/monitoring:"),
-        st.Page("pages/page_four.py", title="Map-Based Visualization",icon=":material/map:"),
-        st.Page("pages/page_five.py", title="Life Stage & Health Trends",icon=":material/monitoring:"),
-        st.Page("pages/page_six.py", title="Island-based Body Mass Analysis",icon=":material/monitoring:")]
+        
+        # defalt icon of materail library
+        
+        # st.Page("pages/page_one.py", title="Home", icon=":material/home:"),
+        # st.Page("pages/page_two.py", title="Correlation between Features", icon=":material/insights:"),
+        # st.Page("pages/page_three.py", title="Analysis based on Bill Length & Bill Depth", icon=":material/insights:"),
+        # st.Page("pages/page_four.py", title="Map-Based Visualization", icon=":material/public:"),
+        # st.Page("pages/page_five.py", title="Life Stage & Health Trends", icon=":material/health_and_safety:"),
+        # st.Page("pages/page_six.py", title="Island-based Body Mass Analysis", icon=":material/fitness_center:"),
+        
+        # Color Icons 
+        st.Page("pages/page_one.py", title="Home", icon="🏠"),
+        st.Page("pages/page_two.py", title="Correlation between Features", icon="📊"),
+        st.Page("pages/page_three.py", title="Bill Length & Depth", icon="📈"),
+        st.Page("pages/page_four.py", title="Map-Based Visualization", icon="🗺️"),
+        st.Page("pages/page_five.py", title="Health Trends", icon="❤️"),
+        st.Page("pages/page_six.py", title="Body Mass Analysis", icon="🏋️"),
+
+        
+        
+        
+        ]
 
 
 pg = st.navigation(pages)
